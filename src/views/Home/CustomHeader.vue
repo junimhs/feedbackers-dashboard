4c49a3567697f49abc7d605f305d26af49a846f3
junimhs <junimhs10@gmail.com>
2021-11-26 23:41:43 -0300

    create home page

---
+++ src/views/Home/CustomHeader.vue
@@ -0,0 +1,56 @@
+<template>
+  <header class="header">
+    <div class="header-group">
+      <div class="flex justify-between items-center py-10">
+        <div class="w-28 lg:w-36">
+          <img class="w-full" src="../../assets/images/logo_white.png" alt="">
+        </div>
+        <div class="flex">
+          <button class="px-6 py-2 font-bold rounded-full text-white focus:outline-none">Crie uma conta</button>
+          <button class="px-6 py-2 bg-white font-bold rounded-full text-brand-main focus:outline-none">Entrar</button>
+        </div>
+      </div>
+      <div class="flex flex-col mt-28">
+        <h1 class="text-4xl font-black text-white">
+          Tenha um feedback. <br>
+          E faca seus clientes mais <br class="hidden lg:inline-block">
+          felizes !
+        </h1>
+        <p class="text-lg text-white font-medium">
+          Receba ideias, reclamacoes e feedbacks com um <br class="hidden lg:inline-block">
+          simples widget na pagina.
+        </p>
+        <div>
+          <button class="px-6 py-2 mt-10 bg-white font-bold rounded-full text-brand-main focus:outline-none">
+            Crie um conta gratis
+          </button>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'CustomHeader'
+}
+</script>
+
+<style lang="postcss" scoped>
+.header {
+  @apply bg-brand-main w-full flex justify-center;
+  height: 700px;
+}
+.header-group {
+  @apply flex flex-col w-4/5 max-w-6xl;
+}
+
+@media (min-height: 640px) {
+  .header-group {
+    background-image: url("../../assets/images/blue_balloons.png");
+    background-size: 628px;
+    background-position: 90% 100%;
+    background-repeat: no-repeat;
+  }
+}
+</style>
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,10 +1,37 @@
 <template>
-  <h1 class="text-brand-main font-black">Aqui e a home</h1>
+  <custom-header @create-account="handleAccountCreate" @login="handleLogin"/>
+  <contact />
+  <footer class="flex justify-center py-10 bg-brand-gray">
+    <p class="font-medium text-center text-gray-800">feedback © 2021</p>
+  </footer>
 </template>
 
 <script>
+import CustomHeader from './CustomHeader'
+import Contact from './Contact'
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
 export default {
-  name: 'index'
+  name: 'index',
+  components: { Contact, CustomHeader },
+  setup () {
+    const router = useRouter()
+
+    onMounted(() => {
+      const token = window.localStorage.getItem('token')
+      if (token) {
+        router.push({ name: 'Feedbacks' })
+      }
+    })
+
+    function handleLogin () {}
+    function handleAccountCreate () {}
+
+    return {
+      handleLogin,
+      handleAccountCreate
+    }
+  }
 }
 </script>
 
